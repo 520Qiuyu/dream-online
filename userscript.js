797bd806424860bc0d4450d36ea06e4f42fcadb0
qiuyu <71555151+520Qiuyu@users.noreply.github.com>
2025-09-05 09:08:37 +0800

    refactor: 优化组件结构，提升代码可读性和可维护性
    将考试、作业和视频功能模块化，分别创建 doExam.js、doHomeWork.js 和 watchVideo.js 文件，优化了按钮样式和布局，增强用户体验。

---
+++ userscript.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         梦想在线
+// @namespace    http://tampermonkey.net/
+// @version      2025-06-30
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.mxdxedu.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=mxdxedu.com
+// @grant        none
+// @require      https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/crypto-js.min.js
+// @require      file:///D:/Documents/dream-online/index.js // index.js文件地址
+// @require      file:///D:/Documents/dream-online/doExam.js // doExam.js文件地址
+// @require      file:///D:/Documents/dream-online/doHomeWork.js // doHomeWork.js文件地址
+// @require      file:///D:/Documents/dream-online/watchVideo.js // watchVideo.js文件地址
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  // Your code here...
+})();
